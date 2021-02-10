456cfa40be3390d2ec6adc8671371abf7a76bd37
Trevor Youngquist <ytrevor81@gmail.com>
2021-02-10 11:25:56 +0700

    delegated transfers complete

--- contracts/TrevToken.sol
+++ contracts/TrevToken.sol
@@ -6,14 +6,20 @@ contract TrevToken {
   string public standard = "Trev Token v1.0"; //Token symbol
   uint256 public totalSupply; //Number of tokens available
 
-  event Transfer(
+  event Transfer (
     address indexed _from,
     address indexed _to,
     uint256 _value
   );
 
+  event Approval (
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+  );
 
   mapping(address => uint256) public balanceOf; //Returns balence of owner via owner address
+  mapping(address => mapping(address => uint256)) public allowance;
 
   constructor(uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -30,4 +36,31 @@ contract TrevToken {
 
     return true;
   }
+
+  //Delegated transfer
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+
+    emit Approval(msg.sender, _spender, _value);
+
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+
+    //Change account balances of sender and receiver
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+
+    //Update allowance
+    allowance[_from][msg.sender] -= _value;
+
+    emit Transfer(_from, _to, _value);
+
+    return true;
+  }
+
+
 }
--- test/TrevToken.js
+++ test/TrevToken.js
@@ -54,5 +54,72 @@ contract('TrevToken', function(accounts){
     });
   });
 
+it('approves token for delegated transfer', function(){
+  return TrevToken.deployed().then(function(instance) {
+    tokenInstance = instance;
+    return tokenInstance.approve.call(accounts[1], 100);
+  }).then(function(success) {
+    assert.equal(success, true, 'equals true');
+    return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+  }).then(function(receipt) {
+    assert.equal(receipt.logs.length, 1, 'triggers one event');
+    assert.equal(receipt.logs[0].event, 'Approval', 'should be the Approve event');
+    assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+    assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+    assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+    return tokenInstance.allowance(accounts[0], accounts[1]);
+  }).then(function(allowance) {
+    assert.equal(allowance, 100, 'stores allowance for delegated transfer');
+  });
+});
+
+  it('handles delegated transfer', function() {
+    return TrevToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      fromAccount= accounts[2];
+      toAccount = accounts[3];
+      spendingAccount = accounts[3];
+      return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+    }).then(function(receipt) {
+      return tokenInstance.approve(spendingAccount, 10, {from: fromAccount });
+    }).then(function(receipt) {
+      return tokenInstance.transferFrom(fromAccount, toAccount, 999, { from: spendingAccount });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than balance');
+      return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than approved amount');
+      return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+    }).then(function(success) {
+      assert.equal(success, true, 'successful approved transfer');
+      return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the transfer event');
+      assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+      assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+      return tokenInstance.balanceOf(fromAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 90, 'deducts the amount from the sending account');
+      return tokenInstance.balanceOf(toAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 10, 'adds the amount to receiving account');
+      return tokenInstance.allowance(fromAccount, spendingAccount);
+    }).then(function(allowance) {
+      assert.equal(allowance, 0, 'deducts amount from the allowance');
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
 
 });
