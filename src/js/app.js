cfed998b63f66d81eaa0b584b669286e51c7dc68
Trevor Youngquist <ytrevor81@gmail.com>
2021-02-12 22:46:02 +0700

    basic html, css, js complete for UI

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
-console.log("Let's get this party started");
+App = {
+  web3Provider: null,
+
+  init: function() {
+    console.log("App initialized")
+    return App.initweb3;
+  },
+
+  initweb3: function() {
+    if(typeof web3 !== 'undefined'){
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+  },
+
+}
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
